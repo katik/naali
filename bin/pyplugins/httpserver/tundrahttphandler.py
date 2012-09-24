8c3e2e69ed98d9f1bfbcb7c9b47c44305f7920d2
antont <toni@playsign>
2012-09-24 16:21:39 +0300

    code to actually show the image (via a separate webserver), and a separate conf file (template)

--- bin/pyplugins/httpserver/tundrahttphandler.py
+++ bin/pyplugins/httpserver/tundrahttphandler.py
@@ -1,10 +1,15 @@
 import sys
 import asyncore
 
+#to move the img file to web server dir
+import shutil
+import os.path
+
 sys.path.append('./pyplugins/httpserver') #omg
 import asynchttp
 
 import tundra
+import config
 
 server = None
 
@@ -16,11 +21,15 @@ class TundraRequestHandler(asynchttp.MyRequestHandler):
     def replycontent(self, f):
         #note: request is in self.path, as e.g. "render/something?x=y"
         shotpath = camera.SaveScreenshot()
+        shutil.move(shotpath, config.IMGDIR)
+        imgurl = config.IMGURLBASE + os.path.basename(shotpath)
 
         f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
         f.write("<html>\n<title>Hello from %s</title>\n" % "Tundra")
         f.write("<body>\n<h2>This is reply from %s</h2>\n" % self)
         f.write("<p>Screenshot was shot to: %s</p>\n" % shotpath)
+        f.write('<img src="%s"/>' % imgurl)
+
 
 def init():
     print "FrameRender camera INIT:"
